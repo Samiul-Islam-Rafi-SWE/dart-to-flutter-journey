fdffc6d4747f1ab54c8597c9d23cbdbcab7d909a
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-05 04:33:21 +0700

    compile and run time

---
+++ compile_and_run_time_error.dart
@@ -0,0 +1,23 @@
+/*  --------Compile Time error---
+void main() {
+  int a = 10 ------>Semicolon error (;)
+  print(a);
+}
+
+
+------Runtime Error-----
+example-1 
+
+void main() {
+  int a = 10;
+  int b = 0;
+  print(a ~/ b);
+}
+
+example-2
+
+void main() {
+  String? name;
+  print(name!.length);
+}
+*/
