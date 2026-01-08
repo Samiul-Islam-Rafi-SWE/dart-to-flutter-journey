25a432d9cac2e54687f09527e08d76f4a81b2659
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-09 03:05:48 +0700

    some add to code if_else condition. How to if-else consition work

--- if_else.dart
+++ if_else.dart
@@ -1,1 +1,22 @@
+//jodi rickshaw pai, then rickshawi kore basai jabo
+//jodi na pai tahoel hete habo
 
+main() {
+  bool gotRickshaw = false;
+  if (gotRickshaw == true) {
+    print("rickshaw kore bazare jabo");
+  } else {
+    print("paye hete bazare jabo");
+  }
+
+  int taka = 10;
+  if (taka > 100) {
+    print("Fuska khabo");
+  } else if (taka >= 50) {
+    print("cha kahbo");
+  } else if (taka >= 30) {
+    print("chips khako");
+  } else {
+    print("khaboi na kisu");
+  }
+}
