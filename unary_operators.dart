1ec8a83b50ea101a43864e5d92fce35b79fd4e59
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-09 02:07:20 +0700

    some operator solveing

--- unary_operators.dart
+++ unary_operators.dart
@@ -1,4 +1,5 @@
 void main() {
+  /*
   double x = 1;
 
   print(x++);
@@ -17,4 +18,31 @@ void main() {
   int m = 20;
   int n = m--;
   print('m : $m, n:$n');
+*/
+
+  int a = 23 + 45;
+  int b = 23 - 4;
+  int c = 334 * 3;
+  double d = 23 / 7;
+  int e = 23 % 2;
+
+  print(a);
+  print(b);
+  print(c);
+  print(d.toStringAsFixed(3));
+  print(e);
+
+  //a = a + 1;
+  //print(a++);
+  //print(a);
+  //a = a- 1;
+  print(a--);
+  print(a);
+
+  a += 10; //a = a + 10;
+  a -= 12; //a = a - 12;
+  a *= 12; //a = a * 12;
+  a = 2; //a = a / 12;
+  a %= 2; //a = a % 12;
+
 }
