f8b9f4113c9e7a2469cac2901663ad0aa0c61524
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-10 23:47:15 +0600

    loop condition

---
+++ loop.dart
@@ -0,0 +1,14 @@
+void main() {
+  for (int i = 1; i <= 6; i++) {
+    print("Daffodil International Univerfsity");
+  }
+  int no = 1;
+  /*do {
+    print("No is $no");
+  } while (no < 50);
+*/
+  while ( no <=10 ) {
+    print("No is $no");
+    no++;
+  }
+}
