ecc0512a05e419319378712474f5211ffd62a9e2
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-12 22:21:15 +0600

    finaly solve

--- function.dart
+++ function.dart
@@ -1,12 +1,29 @@
 //Reusability
 void main() {
   //function call
-  welcomeMessage('rohim','Daffodil International University');
-  welcomeMessage('samiul','IUBAT');
-  welcomeMessage('kamal','BUBT');
+  welcomeMessage(
+    name: 'rohim',
+    university: 'Daffodil International University',
+    age: 12,
+  );
+  welcomeMessage(name: 'samiul', university: 'IUBAT', age: 14);
+  welcomeMessage(name: 'kamal', university: 'BUBT', age: 34);
+  print(add(13, 13.5));
+  double r = add(23, 34);
+  print(r);
 }
+
 //parameters--->(String name)
-welcomeMessage(String name ,String university,[int? age,int? num]) {
+void welcomeMessage({required name, required university, required age}) {
+  //body
   print("Hi Good Morning,$name,$university");
-  print("how are you");
+  print("how are you . now this year your age is $age");
+}
+/*return type--> function name(params){
+body part
+}*/
+
+add(int a, double b) {
+  double result = a + b;
+  return result;
 }
--- statements_exp_operators.dart
+++ statements_exp_operators.dart
@@ -21,5 +21,4 @@ void main() {
 
   // null vlaue
 
-
 }
