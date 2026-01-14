c254b8f0b24abd401748cf419e8e97a74a3d46ec
samiul77RR <157511343+samiul77RR@users.noreply.github.com>
2026-01-15 01:47:11 +0600

    static

--- oop/classes.dart
+++ oop/classes.dart
@@ -8,6 +8,7 @@ void main() {
   studentOne.adderess = 'Daffodil Smart City';
   studentOne.age = 23;
   studentOne.id = 241 - 35 - 430;
+  Student.university = 'DU';
   studentOne.department();
 
   Student studentTwo = Student();
@@ -15,12 +16,16 @@ void main() {
   studentTwo.adderess = "jamalpur";
   studentTwo.age = 34;
   studentTwo.id = 241 - 35 - 345;
+  Student.university = "BUFT";
 
   studentList.add(studentOne);
   studentList.add(studentTwo);
 
-  Teacher biologyTeacher = Teacher(name: 'khirul', age: 12,department:  'SWE');
+  Teacher biologyTeacher = Teacher(name: 'khirul', age: 12, department: 'SWE');
   print(biologyTeacher.name);
+  print(Student.university);
+  print(Student.university);
+  Student.doAnything();
 }
 
 //Student
@@ -30,6 +35,7 @@ class Student {
   String adderess = '';
   int age = 0;
   int id = 0;
+  static String university = "Daffodil International University";
 
   //constructor
   Student() {
@@ -39,6 +45,11 @@ class Student {
   void department() {
     print('$name study to Department $adderess ');
     print("This is $name and location is $adderess");
+  }
+
+  static void doAnything() {
+    print(university);
+    print("anything");
   }
 }
 
